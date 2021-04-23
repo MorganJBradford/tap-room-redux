26e03a5c32b18a854adda055159eafc005e17c86
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 11:01:22 -0500

    remove placeholder text. Return KegList

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Header from "Header";
+import Header from "./Header";
+import KegList from "./KegList";
 
 function App() {
   return (
     <>
       <Header />
-      <h2>Name: {names}</h2>
-      <h2>Name: {brandName}</h2>
-      <h2>Price: {price}</h2>
-      <h2>Alcohol: {booze}</h2>
+      <KegList />
     </>
   );
 }
