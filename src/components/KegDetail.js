fe98abf0ace6c43082ade2eb83311a41bae884ab
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 13:45:23 -0500

    destructure, create button for, and declare proptypes for onClickingDelete

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,20 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function KegDetail(props){
-  const { keg } = props;
+  const { keg, onClickingDelete } = props;
   return (
     <>
       <h1>Keg Detail</h1>
       <h3>{keg.names} - {keg.brand}</h3>
       <p>{keg.price}</p>
       <p><strong>{keg.alcohol}</strong></p>
+      <button onClick={()=> onClickingDelete(keg.id)}>Remove Keg</button>
       <hr />
     </>
   );
 }
 
 KegDetail.propTypes = {
-  keg: PropTypes.object
+  keg: PropTypes.object,
+  onClickingDelete: PropTypes.func
 };
 
 export default KegDetail;
