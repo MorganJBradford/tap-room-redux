e7feaa782c4bc8c34fea5e8ee89ad5beb4251ace
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 10:57:34 -0500

    add placeholder h2s

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,13 @@ import Header from "Header";
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <h2>Name: {names}</h2>
+      <h2>Name: {brandName}</h2>
+      <h2>Price: {price}</h2>
+      <h2>Alcohol: {booze}</h2>
+    </>
   );
 }
 
