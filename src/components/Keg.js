5d3843b1cd3f2c0963f4bb327f6553ccca981ad9
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 15:55:31 -0500

    display availability in h2. Add context text to displayed keg props. add pints proptype. Change price/alcohol proptypes

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -7,8 +7,9 @@ function Keg(props) {
       <div onClick={() => props.whenKegClicked(props.id)}>
         <h2>Name: {props.names}</h2>
         <h2>Brand: {props.brand}</h2>
-        <h2>Price: {props.price}</h2>
-        <h2>Alcohol: {props.alcohol}</h2>
+        <h2>Price: ${props.price}/pint</h2>
+        <h2>Alcohol: {props.alcohol}%</h2>
+        <h2>Availability: {props.pints}pts</h2>
         <hr/>
       </div>
     </>
@@ -18,8 +19,9 @@ function Keg(props) {
 Keg.propTypes = {
   names: PropTypes.string,
   brand: PropTypes.string,
-  price: PropTypes.string,
-  alcohol: PropTypes.string,
+  price: PropTypes.number,
+  alcohol: PropTypes.number,
+  pints: PropTypes.number,
   id: PropTypes.string,
   whenKegClicked: PropTypes.func
 };
