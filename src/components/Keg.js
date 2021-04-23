de5047b032b06580592f61e4e5c941a9061b4e13
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 11:06:11 -0500

    correct using statements; define placeholder variables

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 function Keg() {
+  const names = "Jerry";
+  const brandName = "Jack";
+  const price = "$15";
+  const booze = "Jerry";
   return (
     <>
       <h2>Name: {names}</h2>
-      <h2>Name: {brandName}</h2>
+      <h2>Brand: {brandName}</h2>
       <h2>Price: {price}</h2>
       <h2>Alcohol: {booze}</h2>
     </>
