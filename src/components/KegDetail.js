1fe87e8242bedaa255252501ee46c2a44ba96955
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 12:49:57 -0500

    destructure keg; dynamically display keg properties; declare keg proptypes

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -1,12 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function KegDetail(props){
+  const { keg } = props;
   return (
     <>
       <h1>Keg Detail</h1>
+      <h3>{keg.names} - {keg.brand}</h3>
+      <p>{keg.price}</p>
+      <p><strong>{keg.alcohol}</strong></p>
       <hr />
     </>
   );
 }
+
+KegDetail.propTypes = {
+  keg: PropTypes.object
+};
 
 export default KegDetail;
