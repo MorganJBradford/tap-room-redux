4d0551fe38f3c82af38b661291f6b282bb9dd6aa
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 11:01:56 -0500

    return keg

---
+++ src/components/Keg.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Keg() {
+  return (
+    <>
+      <h2>Name: {names}</h2>
+      <h2>Name: {brandName}</h2>
+      <h2>Price: {price}</h2>
+      <h2>Alcohol: {booze}</h2>
+    </>
+  );
+}
+
+export default Keg;
