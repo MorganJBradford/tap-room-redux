07ab0324202b1faa6e2804d41ee12cf5258e0b16
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-23 14:12:19 -0500

    destructure, create button for, and set proptype for onClickingEdit

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -2,13 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function KegDetail(props){
-  const { keg, onClickingDelete } = props;
+  const { keg, onClickingDelete, onClickingEdit } = props;
   return (
     <>
       <h1>Keg Detail</h1>
       <h3>{keg.names} - {keg.brand}</h3>
       <p>{keg.price}</p>
       <p><strong>{keg.alcohol}</strong></p>
+      <button onClick={ onClickingEdit }>Edit Keg</button>
       <button onClick={()=> onClickingDelete(keg.id)}>Remove Keg</button>
       <hr />
     </>
@@ -17,7 +18,8 @@ function KegDetail(props){
 
 KegDetail.propTypes = {
   keg: PropTypes.object,
-  onClickingDelete: PropTypes.func
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
 };
 
 export default KegDetail;
