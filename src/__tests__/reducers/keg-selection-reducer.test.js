1c824ae486208fc0bb9157f021f53f654f8cfed0
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 18:32:18 -0500

    write test should return selected keg; passes test

--- src/__tests__/reducers/keg-selection-reducer.test.js
+++ src/__tests__/reducers/keg-selection-reducer.test.js
@@ -1,8 +1,30 @@
 import kegSelectionReducer from "../../reducers/keg-selection-reducer";
+import * as c from "../../actions/ActionTypes";
 
 describe("kegSelectionReducer", () => {
 
-  test('Should return default state if no action type is recognized', () => {
-    expect(kegSelectionReducer(null, { type: null })).toEqual(null);
+  const testData = {
+    type: c.SELECT_KEG,
+    names: "Righteous Whiskey",
+    brand: "Brandolino's",
+    price: 20,
+    alcohol: 45,
+    pints: 100,
+    id: 1
+  };
+
+  test("Should return default state if no action type is recognized", () => {
+    expect(kegSelectionReducer({}, { type: null })).toEqual({});
+  });
+
+  test("should return selected keg", () => {
+    expect(kegSelectionReducer({}, testData)).toEqual({
+      names: "Righteous Whiskey",
+      brand: "Brandolino's",
+      price: 20,
+      alcohol: 45,
+      pints: 100,
+      id: 1
+    });
   });
 });
