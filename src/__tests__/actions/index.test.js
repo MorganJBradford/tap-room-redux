cd96415f8628eca470668edada0643c3cfb6efce
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 11:39:28 -0500

    correct typo in test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -3,7 +3,7 @@ import * as c from "./../../actions/ActionTypes"
 
 describe("tap-room actions", () => {
   it("deleteKeg should create DELETE_KEG action", () => {
-    expect(actions.deleteKeg(1)).toEqual({
+    expect(a.deleteKeg(1)).toEqual({
       type: c.DELETE_KEG,
       id: 1
     })
