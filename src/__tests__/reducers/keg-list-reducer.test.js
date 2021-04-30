2294a07c04e0c53bdeeee5ce5e211deea75a2f77
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 11:06:27 -0500

    import action consts as c. Good fail: add new keg data test

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -1,7 +1,43 @@
 import kegListReducer from "../../reducers/keg-list-reducer";
+import * as c from "../../actions/ActionTypes";
 
 describe("kegListReducer", () => {
+
+  let action;
+  const kegData = {
+  names: "Righteous Whiskey",
+  brand: "Brandolino's",
+  price: 20,
+  alcohol: 45,
+  pints: 100,
+  id: 1,
+  };
+
   test("Should return default state if there is no action type passed into the reducer", () => {
     expect (kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test("Should successfully add new keg data to mainKegList", () => {
+    const { names, brand, price, alcohol, pints, id, } = kegData;
+    action = {
+      type: c.ADD_KEG,
+      names: names,
+      brand: brand,
+      price: price,
+      alcohol: alcohol,
+      pints: pints,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        names: names,
+        brand: brand,
+        price: price,
+        alcohol: alcohol,
+        pints: pints,
+        id: id
+      }
+    });
   });
 });
