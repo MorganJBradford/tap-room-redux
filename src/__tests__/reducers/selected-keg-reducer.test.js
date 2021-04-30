8960cc537acc2a6b6d9982cdee190312001761d0
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 14:03:36 -0500

    create test: return default state; good fail

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -1,12 +1,8 @@
-import * as c from "../../actions/ActionTypes";
-import editingKegReducer from "../../reducers/editing-keg-reducer";
+import selectedKegReducer from "../../reducers/selected-keg-reducer";
 
-describe("editingKegReducer", () => {
+describe("selectedKegReducer", () => {
+  
   test("Should return default state if no action type is recognized", () => {
-    expect(editingKegReducer(false, { type: null })).toEqual(false);
-  });
-
-  test("Should toggle editing keg state to true", () => {
-    expect(editingKegReducer(false, { type: c.TOGGLE_EDIT_KEG })).toEqual(true);
+    expect(selectedKegReducer(null, { type: null })).toEqual(null);
   });
 });
