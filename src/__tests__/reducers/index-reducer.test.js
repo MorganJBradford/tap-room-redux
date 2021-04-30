5a07b2e8d053e770ba869f910fddf3d0bb0f8b42
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 12:31:34 -0500

    create and auto pass tests that compare rootReducer default states to the main reducers

--- src/__tests__/reducers/index-reducer.test.js
+++ src/__tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
+import { createStore } from "redux";
+import formVisibleReducer from "../../reducers/form-visible-reducer";
 import rootReducer from "../../reducers/index";
+import kegListReducer from "../../reducers/keg-list-reducer";
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
 
@@ -7,6 +12,14 @@ describe("rootReducer", () => {
       mainKegList: {},
       formVisibleOnPage: false
     });
+  });
+
+  test("Check initial state of kegListReducer matches rootReducer", () => {
+    expect(store.getState().mainKegList).toEqual(kegListReducer(undefined, { type: null }));
+  });
+
+  test("Check that initial state of formVisibleReducer matches rootReducer", () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
   });
 
 });
