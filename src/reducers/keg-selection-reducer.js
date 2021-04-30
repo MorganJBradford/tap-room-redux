7c3fdef2047e8470bfc2ee85ba1bfac439453c4b
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 18:02:43 -0500

    define keg selection reducer

--- src/reducers/keg-selection-reducer.js
+++ src/reducers/keg-selection-reducer.js
@@ -1,3 +1,17 @@
-export default (state = null, action) => {
-  return state;
+import * as c from "../actions/ActionTypes";
+export default (state = {}, action) => {
+  const { names, brand, price, alcohol, pints, id } = action;
+  switch (action.type) {
+    case c.SELECT_KEG:
+      return Object.assign({}, {
+          names: names,
+          brand: brand,
+          price: price,
+          alcohol: alcohol,
+          pints: pints,
+          id: id
+      });
+    default:
+      return state;
+  }
 };
