bddd6ce8e4fa63cb5b37c693dcfd5af65b999263
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 13:12:13 -0500

    write and auto-pass test: return default state

---
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -0,0 +1,8 @@
+import * as c from "../../actions/ActionTypes";
+import selectedKegReducer from "../../reducers/selected-keg-reducer";
+
+describe("selectedKegReducer", () => {
+  test("Should return default state if no action type is recognized", () => {
+    expect(selectedKegReducer(false, { type: null })).toEqual(false);
+  });
+});
