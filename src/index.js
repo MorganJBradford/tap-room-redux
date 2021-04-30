f002c0af56b1c843e496e63e9699120125632002
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 11:29:46 -0500

    import redux store and provider. Wrap <App/> in provider tags. Pass store as provider attribute

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
+import { createStore } from "redux";
+import { Provider } from "react-redux"
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
+import './index.css';
+import reducer from "./reducers/keg-list-reducer";
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
