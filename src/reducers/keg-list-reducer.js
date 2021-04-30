b95a06e498063d2b8b69ee090281c1a442ffbc60
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 11:10:24 -0500

    import action constants. Pass add keg test

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,20 @@
+import * as c from "./../actions/ActionTypes";
+
 export default (state = {}, action) => {
-  return state;
+  const { names, brand, price, alcohol, pints, id } = action;
+  switch (action.type) {
+    case c.ADD_KEG:
+      return Object.assign({}, state, {
+        [id]: {
+          names: names,
+          brand: brand,
+          price: price,
+          alcohol: alcohol,
+          pints: pints,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
