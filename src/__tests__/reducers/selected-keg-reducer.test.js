2348bf2cadd7c9e67f72ec182aaee03fd54dc688
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 13:15:50 -0500

    write test: toggle editting to true; good fail

--- src/__tests__/reducers/selected-keg-reducer.test.js
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -5,4 +5,8 @@ describe("selectedKegReducer", () => {
   test("Should return default state if no action type is recognized", () => {
     expect(selectedKegReducer(false, { type: null })).toEqual(false);
   });
+
+  test("Should toggle edit keg state to true", () => {
+    expect(selectedKegReducer(false, { type: c.TOGGLE_EDIT_KEG })).toEqual(true);
+  });
 });
