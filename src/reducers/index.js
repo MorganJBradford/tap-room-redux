f54ed19677d350060e333fc33a5043eb5b65b4d9
Morgan Bradford <morganjbradford95@gmail.com>
2021-04-30 18:03:47 -0500

    import selectedKeg and add to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import editingKegReducer from "./editing-keg-reducer";
 import formVisibleReducer from "./form-visible-reducer";
 import kegListReducer from "./keg-list-reducer";
+import kegSelectionReducer from "./keg-selection-reducer";
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
   mainKegList: kegListReducer,
   editingKeg: editingKegReducer,
+  selectedKeg: kegSelectionReducer
 });
 
 export default rootReducer;
